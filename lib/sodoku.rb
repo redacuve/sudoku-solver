13bedd8ec7d42e0fef68a37c36756e7b41a3c6f6
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-22 00:53:37 -0600

    added posible number method

--- lib/sodoku.rb
+++ lib/sodoku.rb
@@ -6,4 +6,23 @@ class Sodoku
   def show
     @sodoku.each { |e| print "#{e}\n" }
   end
+
+  def posible?(y, x, n)
+    return false unless @soduko[y][x].zero?
+
+    9.times do |i|
+      return false if @soduko[y][i] == n
+    end
+
+    9.times do |i|
+      return false if @soduko[i][x] == n
+    end
+
+    ((3 * (y / 3))..(3 * (y / 3) + 2)).each do |yy|
+      ((3 * (x / 3))..(3 * (x / 3) + 2)).each do |xx|
+        return false if @soduko[yy][xx] == n
+      end
+    end
+    true
+  end
 end
