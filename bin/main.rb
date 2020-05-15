3275aa8e1fe98bae2202dc99363dd1560b8358bb
Rey David C.V <cuevas.vela@gmail.com>
2020-05-14 23:17:16 -0500

    fixed a little typo on the load of the CSV file

--- bin/main.rb
+++ bin/main.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/sudoku.rb'
 require 'csv'
 
-sudoku = CSV.read('lib/sudoku.csv').each { |line| line.map!(&:to_i) }
+sudoku = CSV.read('../lib/sudoku.csv').each { |line| line.map!(&:to_i) }
 my_sudoku = Sudoku.new(sudoku)
 my_sudoku.show
 my_sudoku.solve
